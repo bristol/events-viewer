3ed10c2238fba567700ff9a90491f0c7696f2df6
Louis Taylor <louis@kragniz.eu>
2019-10-29 18:38:33 +0000

    Parse a bunch of meetup data

--- src/templates/Event.vue
+++ src/templates/Event.vue
@@ -3,6 +3,7 @@
     <div>
       <h1 v-html="$page.event.title" />
       <div v-html="$page.event.content" />
+      <p v-html="$page.event.description" />
     </div>
   </Layout>
 </template>
@@ -11,6 +12,7 @@
 query Event ($path: String!) {
    event: event (path: $path) {
     title
+    description
   }
 }
 </page-query>
