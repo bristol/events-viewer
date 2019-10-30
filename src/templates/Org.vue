700de18991274195075063513a854a007accb3fc
Louis Taylor <louis@kragniz.eu>
2019-10-30 11:20:02 +0000

    Add org view

---
+++ src/templates/Org.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <div>
+      <h1 v-html="$page.org.name" />
+      <div v-html="$page.org.id" />
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Org ($path: String!) {
+   org: org (path: $path) {
+    id
+    name
+  }
+}
+</page-query>
